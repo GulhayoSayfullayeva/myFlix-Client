27e84f62b2a2e81231cef70c4c80d41eabbcac9d
GulhayoSayfullayeva <gulhayo.sayfullayeva@gmail.com>
2023-08-01 20:18:34 +0200

    login&signup

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,64 @@
+import "./login-view.css";
+import { Register } from "../register/register.jsx";
+import { useState } from "react";
+import { Test } from "../test/test";
+
+export const LoginView = ({ onLoginSubmit }) => {
+   const [userName, setUserName] = useState("");
+   const [pass, setPass] = useState("");
+   const [clicked, setClicked] = useState(false);
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      const data = {
+         username: userName,
+         password: pass
+      };
+      fetch("https://myflix-h3mr.onrender.com/login", {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify(data)
+      }).then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            if (data.user) {
+               localStorage.setItem("user", JSON.stringify(data.user.username));
+               localStorage.setItem("token", data.token);
+               onLoginSubmit(data.user.username, data.token);
+            }
+            else {
+               alert("Login failed");
+            }
+         });
+
+   };
+   if (!clicked) {
+      console.log(clicked);
+      return (<div className="login">
+         <h1>Login</h1>
+         <form className="form" onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input type="text" name="username" placeholder="username" value={userName} onChange={(e) => setUserName(e.target.value)} required></input>
+            <label>Password</label>
+            <input type="password" name="password" placeholder="password" value={pass} onChange={(e) => setPass(e.target.value)} required></input>
+            <button type="submit">Submit</button>
+            <div className="signup">Don't have account?<br />
+               <div onClick={() => {
+                  console.log("signup clicked");
+                  setClicked(true);
+               }}>Signup</div>
+            </div>
+         </form>
+      </div>);
+   }
+   else {
+      console.log(clicked);
+      return(
+      <div>
+         <Register afterRegis={() => setClicked(false)} />
+      </div>);
+   }
+
+
+
+}
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,49 +1,51 @@
 import PropTypes from 'prop-types';
+import "./movie-view.css"
 
-export const MovieView = ({movie, onBackClick}) => {
-    return(
-        <div>
+export const MovieView = ({ movie, onBackClick }) => {
+    return (
+        <div className="details-page">
             <div>
-                <img src={movie.image}/>
+                <img src={movie.image} />
             </div>
-            <div>
-                <span>Title:</span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Description:</span>
-                <span>{movie.description}</span>
-            </div>
-            <div>
-                <span>Director:</span>
-                <span>{movie.director.name}</span>
-            </div>
-            <div>
-                <span>Genre:</span>
-                <span>{movie.genre.name}</span>
-            </div>
-            <div>
+            <div className="details">
+                <div className="movie-details">
+                    <span className="bold">Title: </span>
+                    <span className="normal">{movie.title}</span><br />
+                </div>
+                <div className="movie-details">
+                    <span className="bold">Description: </span>
+                    <span className="normal">{movie.description}</span><br />
+                </div>
+                <div className="movie-details">
+                    <span className="bold">Director: </span>
+                    <span className="normal">{movie.director.name}</span><br />
+                </div>
+                <div className="movie-details">
+                    <span className="bold">Genre: </span>
+                    <span className="normal">{movie.genre.name}</span><br />
+                </div>
+
                 <button onClick={onBackClick}>Back</button>
             </div>
-            
+
         </div>
     );
 
 };
 
 MovieView.propTypes = {
-    movie : PropTypes.shape({
-       image : PropTypes.string.isRequired,
-       title : PropTypes.string.isRequired,
-       description : PropTypes.string.isRequired,
-       director : PropTypes.shape({
-          name : PropTypes.string.isRequired,
-          bio : PropTypes.string
-       }),
-       genre : PropTypes.shape({
-          name : PropTypes.string.isRequired,
-          description : PropTypes.string
-       })
+    movie: PropTypes.shape({
+        image: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bio: PropTypes.string
+        }),
+        genre: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string
+        })
     }),
-    onBackClick : PropTypes.func.isRequired
+    onBackClick: PropTypes.func.isRequired
 };
